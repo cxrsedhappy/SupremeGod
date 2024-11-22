9303486c704d12dc9ac9c808cb1d0b274694dae0
Stanislaw <cxrsedhappy@mail.ru>
2024-11-22 03:14:06 +0300

    Added keyboard events and improved GUI render

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import enum
+# Made by cxrsedhappy with love and tears
+# I have uni-code chars...
+# 22.11.2024 RIP
+
 import os
-import random
+import enum
 import time
 import torch
+import random
+import keyboard
 
 from colorama import Fore
-from sympy.core.random import randint
 from ascii_text import ShadedBlockyFont
 
 
@@ -15,40 +19,109 @@ ASCII_NUM_BACKGROUND = ord('░')
 ASCII_NUM_SNAKE = ord('▒')
 ASCII_NUM_APPLE = ord('█')
 
+
 class HeadDirection(enum.Enum):
     UP: tuple[int, int] = (0, 1)
     RIGHT: tuple[int, int] = (1, 0)
     DOWN: tuple[int, int] = (0, -0)
     LEFT: tuple[int, int] = (-1, 0)
 
+    def __str__(self):
+        return self.name
+
+
 class GUI:
     def __init__(self):
-        self.title = ShadedBlockyFont().render('supremegod')
+        self.__title = ShadedBlockyFont().render('supremegod')
         self.__device_name: str = "cuda" if torch.cuda.is_available() else "cpu"
         self.__torch_version = torch.__version__
+        self.__style = {
+            'border_h': '=',
+            'border_v': '║',
+            'corner': '╬',
+            'padding': 1,
+            'fc_on': Fore.GREEN,
+            'fc_off': Fore.RED,
+            'fc_reset': Fore.RESET
+        }
 
-    def render_title(self):
-        print(self.title)
+    def __render_title(self):
+        print(self.__title)
 
-    def render_params(self, ai_mode: bool, direction: tuple[int, int]):
-        """TODO: Complete parameters text"""
-        print(
-            '+' + '-' * 45 + '+' + '-' * 15 + "+" + '-' * 27 + "+\n" +
-            f'| Torch version {Fore.GREEN + self.__torch_version + Fore.RESET:>8} using device {Fore.GREEN + self.__device_name + Fore.RESET:>4} | AI Mode {str(ai_mode):<5} | Head direction {str(direction):<10} |\n' +
-            '+' + '-' * 45 + '+' + '-' * 15 + '+' + '-' * 27 + "+\n"
+    def __render_params(self, ai_mode: bool, direction: tuple[int, int]):
+        """
+        Renders parameter information in a formatted table.
+
+        Args:
+            ai_mode (bool): Status of AI mode
+            direction (tuple[int, int]): Current head direction
+        """
+
+        ai_option_color = self.__style["fc_on"] if ai_mode else self.__style["fc_off"]
+
+        out = []
+        labels = [' Torch version: ', ' AI mode: ', ' Direction: ']
+
+        # Workaround to bypass shitty uni-code colours. I WANT COLOURS SO MUCH.
+        # Contains (text, color uni-code offset)
+        # p.s i hate uni-code colours
+        values: list[tuple[str, int]] = [
+            (
+                self.__style["fc_on"] + self.__torch_version + self.__style["fc_reset"],
+                len(self.__style["fc_on"] +  self.__style["fc_reset"]))
+            ,
+            (
+                ai_option_color + str(ai_mode) + self.__style["fc_reset"],
+                len(self.__style["fc_off"] +  self.__style["fc_reset"])
+            ),
+            (
+                str(direction),
+                0
+            )
+        ]
+
+        total_widths = [50, 20, 25]
+        value_widths = [total_widths[i] - len(labels[i]) + values[i][1] for i in range(len(labels))]
+
+        content = []
+        for i in range(len(labels)):
+            content.append(f'{labels[i]}' + f'{values[i][0]:^{value_widths[i]}}')
+
+        out.append(
+            self.__style['corner'] +
+            self.__style['corner'].join(self.__style["border_h"] * total_widths[i] for i in range(len(labels))) +
+            self.__style['corner']
         )
+
+        out.append(
+            f"{self.__style['border_v']}" +
+            self.__style['border_v'].join(content) +
+            f"{self.__style['border_v']}"
+        )
+
+        out.append(
+            self.__style['corner'] +
+            self.__style['corner'].join(self.__style["border_h"] * total_widths[i] for i in range(len(labels))) +
+            self.__style['corner']
+        )
+
+        print('\n'.join(out))
+
+    def set_style(self, **kwargs):
+        """Updates style configuration"""
+        self.__style.update(kwargs)
 
     def render_frame(self, ai_mode: bool, board: list[str], head_direction: tuple[int, int]):
         """
-        Main method of GUI.
+        Render method of GUI.
         Applies rendering in order:
         - Title
         - Parameters
         - Board
         """
         self.clear_frame()
-        self.render_title()
-        self.render_params(ai_mode, head_direction)
+        self.__render_title()
+        self.__render_params(ai_mode, head_direction)
         for row in board:
             print(row)
 
@@ -63,36 +136,47 @@ class Game:
         """
         Main game class. Process keyboard event, GUI frame render, in-game functionality
         """
-        self.gui = gui
-        self.ai_mode = False
-        self.__running = False
-        self.__board = [
-            ''.join([chr(ASCII_NUM_BACKGROUND) for _ in range(WIDTH)]) for _ in range(HEIGHT)
-        ]
+        self.__gui = gui
+        self.__ai_mode = False
+
+        self.__board = [''.join([chr(ASCII_NUM_BACKGROUND) for _ in range(WIDTH)]) for _ in range(HEIGHT)]
+
         self.__has_apple_eaten = False
-        self.__head_pos: tuple[int, int] = (randint(0, WIDTH), randint(0, HEIGHT))
+        self.__head_pos: tuple[int, int] = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
         self.__head_direction: tuple[int, int] = (1, 0)
-        # keyboard.hook(lambda e: self.__on_btn_action(e))
 
-    # def __on_btn_action(self, e: keyboard):
-    #     if e.event_type == keyboard.KEY_DOWN:
-    #         if e.name == 's':
-    #             self.__head_direction = HeadDirection.DOWN
-    #         if e.name == 'a':
-    #             self.__head_direction = HeadDirection.LEFT
-    #         if e.name == 'w':
-    #             self.__head_direction = HeadDirection.UP
-    #         if e.name == 'd':
-    #             self.__head_direction = HeadDirection.RIGHT
+        self.__running = True
+        self.__accept_input = True
+        keyboard.hook(self.__on_btn_action)
 
-    def __change_ai_mode(self):
-        self.ai_mode = not self.ai_mode
+    def __on_btn_action(self, e: keyboard.KeyboardEvent):
+        if e.event_type == keyboard.KEY_DOWN:
+            match e.name:
+                case 's':
+                    self.__head_direction = HeadDirection.DOWN
+                case 'a':
+                    self.__head_direction = HeadDirection.LEFT
+                case 'w':
+                    self.__head_direction = HeadDirection.UP
+                case 'd':
+                    self.__head_direction = HeadDirection.RIGHT
+                case 'p':
+                    self.__ai_mode = not self.__ai_mode
+                case 'q':
+                    self.__running = False
+
+    def __unhook_on_btn_action(self):
+        keyboard.unhook(self.__on_btn_action)
 
     def render(self):
         """Process render to the screen"""
-        while True:
-            self.gui.render_frame(self.ai_mode, self.__board, self.__head_direction)
+        while self.__running:
+            self.update()
+            self.__gui.render_frame(self.__ai_mode, self.__board, self.__head_direction)
             time.sleep(0.1)
+
+        self.__unhook_on_btn_action()
+
 
     def is_border(self, x: int, y: int):
         """Checks if x, y coords at the border"""
@@ -103,7 +187,7 @@ class Game:
         ...
 
     @staticmethod
-    def gather_empty_space(self) -> list[tuple[int, int]]:
+    def gather_empty_space() -> list[tuple[int, int]]:
         empty_spaces = []
         for i in range(WIDTH):
             for j in range(0, HEIGHT):
@@ -119,7 +203,7 @@ class Game:
 
     def update(self):
         """Process pre-render functionality"""
-        if not self.ai_mode:
+        if not self.__ai_mode:
             ...
 
 
